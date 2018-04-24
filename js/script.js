fef45719d422b69a92d9dc1d45dbb3dd2736e43d
Magdalena Staliś <magdalenastalis@gmail.com>
2018-04-24 13:07:21 +0200

    Fixed problems with decimal and negative value

--- js/script.js
+++ js/script.js
@@ -3,14 +3,13 @@ var vatElement = document.querySelector("#vat");
 var zusElement = document.querySelector("#zus");
 var zdrowotneElement = document.querySelector("#zdrowotne");
 var nettoElement = document.querySelector("#netto");
+var profitElement = document.querySelector("#profit");
+var errorElement = document.querySelector("#warning");
 
 var calculateButton = document.querySelector("#calculate-btn");
-var profitElement = document.querySelector("#profit");
 
 
-var error = document.querySelector("#info");
 
-//value in nettoEElement should be bigger then 0
 
 
 
@@ -19,27 +18,42 @@ var error = document.querySelector("#info");
 calculateButton.addEventListener("click", function () {
     var nettoValue = nettoElement.value;
 
-    if (nettoElement < 0) {
-        error.classList.remove("hidden");
+    if (nettoValue <= 0) {
+        errorElement.classList.remove("hidden");
     }
-    else {
-    
-    var incomeValue = 0.19;
-    var zusValue = 843.45;
-    var zdrowotneValue = 319.94;
-    var goodtax = 0.23;
-    var zdrowotneDoOdjęciaValue = (Math.round(zdrowotneValue * 7.75 / 9 * 100)) / 100;
-    var pitValue = (Math.round((nettoValue * incomeValue) - zdrowotneDoOdjęciaValue)).toFixed(2);
-    var vatValue = (Math.round(nettoValue * goodtax)).toFixed(2);
-    var profitValue = (nettoValue - (pitValue + zusValue + zdrowotneValue)).toFixed(2);
 
-    pitElement.innerText = pitValue + " zł";
-    vatElement.innerText = vatValue + " zł";
-    zusElement.innerText = zusValue + " zł";
-    zdrowotneElement.innerText = zdrowotneValue + " zł";
-    profitElement.innerText = profitValue + " zł";
-}
+    else {
+        var incomeValue = 0.19;
+        var zusValue = 843.45;
+        var zdrowotneValue = 319.94;
+        var goodtax = 0.23;
+        var zdrowotneDoOdjęciaValue = (Math.round(zdrowotneValue * 7.75 / 9 * 100)) / 100;
+        var taxbase = Math.round(nettoValue - zusValue);
+        var vatValue = Math.round(nettoValue * goodtax);
+        var pitValue = Math.round((taxbase * incomeValue) - zdrowotneDoOdjęciaValue);
+
+        if (pitValue < 0) {
+            pitValue = 0;
+        }
+
+        var profitValue = (nettoValue - (pitValue + zusValue + zdrowotneValue));
+
+        if (profitValue < 0) {
+            profitValue = 0;
+        }
+
+        pitElement.innerText = pitValue.toFixed(2) + " zł";
+        vatElement.innerText = vatValue.toFixed(2) + " zł";
+        zusElement.innerText = zusValue + " zł";
+        zdrowotneElement.innerText = zdrowotneValue + " zł";
+        profitElement.innerText = profitValue.toFixed(2) + " zł";
+
+        errorElement.classList.add("hidden");
+    }
 });
+
+
+
 
 
 
