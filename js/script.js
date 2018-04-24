cf9006794efbdce1cd0f77a7d35aeed5f69d7bf5
Magdalena Staliś <magdalenastalis@gmail.com>
2018-04-24 10:52:40 +0200

    Add currency and decimal places

--- js/script.js
+++ js/script.js
@@ -3,10 +3,19 @@ var vatElement = document.querySelector("#vat");
 var zusElement = document.querySelector("#zus");
 var zdrowotneElement = document.querySelector("#zdrowotne");
 var nettoElement = document.querySelector("#netto");
+
 var calculateButton = document.querySelector("#calculate-btn");
 var profitElement = document.querySelector("#profit");
 
+// function validateValue () {
+//     nettoElement => 0;
+//     alert("Kwota musi być większa od 0");
+// };
+
+
+
 //calculate value in the table
+
 
 calculateButton.addEventListener("click", function () {
     var nettoValue = nettoElement.value;
@@ -15,15 +24,18 @@ calculateButton.addEventListener("click", function () {
     var zdrowotneValue = 319.94;
     var goodtax = 0.23;
     var zdrowotneDoOdjęciaValue = (Math.round(zdrowotneValue * 7.75 / 9 * 100)) / 100;
-    var pitValue = Math.round((nettoValue * incomeValue) - zdrowotneDoOdjęciaValue);
-    var vatValue = nettoValue * goodtax;
-    var profitValue = (nettoValue - (pitValue + zusValue + zdrowotneValue));
+    var pitValue = (Math.round((nettoValue * incomeValue) - zdrowotneDoOdjęciaValue)).toFixed(2);
+    var vatValue = (Math.round(nettoValue * goodtax)).toFixed(2);
+    var profitValue = (nettoValue - (pitValue + zusValue + zdrowotneValue)).toFixed(2);
 
-    pitElement.innerText = pitValue;
-    vatElement.innerText = vatValue;
-    zusElement.innerText = zusValue;
-    zdrowotneElement.innerText = zdrowotneValue;
-    profitElement.innerText = profitValue;
+    
+
+    
+    pitElement.innerText = pitValue + " zł";
+    vatElement.innerText = vatValue + " zł";
+    zusElement.innerText = zusValue + " zł";
+    zdrowotneElement.innerText = zdrowotneValue + " zł";
+    profitElement.innerText = profitValue + " zł";
 
 });
 
